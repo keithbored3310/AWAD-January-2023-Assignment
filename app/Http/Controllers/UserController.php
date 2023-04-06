a1a4c334cb3087126be6444a1f7e835ff57456c8
vincenttan0820 <86709944+vincenttan0820@users.noreply.github.com>
2023-04-07 00:08:19 +0800

    Update and rename HomeController.php to UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 
-class HomeController extends Controller
+class UserController extends Controller
 {
     /**
      * Create a new controller instance.
