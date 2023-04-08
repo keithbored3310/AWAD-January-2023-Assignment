99848f72ea935a36a9073cdbd285f6720b468472
tkk19 <kiakheng@1utar.my>
2023-04-08 13:33:18 +0800

    update menu blade

--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,8 @@ Route::get('/menu', [HomeController::class, 'showMenu'])->name('showMenu');
 Route::post('/menu', [HomeController::class, 'filterCategory'])->name('filterCategory');
 
 //OrderController
-Route::get('/cart/{id}', [OrderController::class, 'showCart'])->name('showCart');
-Route::get('/history/{id}', [OrderController::class, 'showHistory'])->name('showHistory');
+Route::get('/cart', [OrderController::class, 'showCart'])->name('showCart');
+Route::get('/history', [OrderController::class, 'showHistory'])->name('showHistory');
 Route::post('/addCart/{id}', [OrderController::class, 'addToCart'])->name('addToCart');
 Route::post('/updateCart/{id}', [OrderController::class, 'updateCart'])->name('updateCart');
 Route::post('/checkout/{id}', [OrderController::class, 'checkout'])->name('checkout');
