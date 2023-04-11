45dfc5bd46d2f93ba2141c8d1302785d56720f8d
keithbored3310 <keithkeong3310@gmail.com>
2023-04-11 10:18:56 +0800

    Upload sidebar page

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -61,6 +61,7 @@
         <div id="myDropdown" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Menu Management:</h6>
+                <a class="collapse-item" href="{{ route('menu.master') }}">Menu List</a>            
                 <a class="collapse-item" href="{{ route('category.master') }}">Category List</a>
             </div>
         </div>
