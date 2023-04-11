e1047cd9e89cc60df7322f4e65affbdfad6cd9a9
keithbored3310 <keithkeong3310@gmail.com>
2023-04-11 22:39:01 +0800

    update show history page

--- resources/views/showHistory.blade.php
+++ resources/views/showHistory.blade.php
@@ -28,7 +28,7 @@
                 <div class="card shadow" style="border-radius: 15px;">
                     <div class="row ">
                         <div class="col-md-4">
-                        <img class="card-img-top" src="{{ asset('images/' . $item->id . '.jpg') }}" alt="Card image cap"> 
+                        <img class="card-img-top" src="{{ asset('images/' . $item->images) }}" alt="Card image cap"> 
                                                <!--{{ asset('images/' . $item->images) }} this one is the code want to replace the src  -->
                         </div>
                         <div class="col-md-8">
